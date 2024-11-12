1d0390e33d1bdf6c894432c5f98226d4423a4135
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-11-12 14:20:30 +0100

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,7 +15,7 @@ $finder = Finder::create()
 
 return (new Config())
     ->setRules([
-        '@PSR2' => true,
+        '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
